f98d95fbd8de1bd4422f27e3993363edbf3968b0
Fiona Burrows <fiona@justfiona.com>
2011-04-28 17:46:29 +0100

    Webcam filter now functions with opencv 2.2

--- pycatcher/filters/webcam.py
+++ pycatcher/filters/webcam.py
@@ -5,48 +5,58 @@ By Fiona Burrows <fiona@myrmidonprocess.com>
 Filter file: Webcam displaying
 """
 
-import opencv
-from opencv import highgui
+import cv, Image
+from ImageDraw import Draw
+
 
 class image_filter(object):
 
-	config = {
-		'width' : 50,
-		'height' : 50,
-		'coordinates' : (10,10),
-		'border_colour' : "#fff"
-		}
+    config = {
+        'width' : 100,
+        'height' : 100,
+        'coordinates' : (10,10),
+        'border_colour' : "#fff"
+        }
 
-	camera = None
+    camera = None
 
-	def __init__(self):
-		self.camera = highgui.cvCreateCameraCapture(0)
-		
-	
-	def apply_filter(self, image, _config):
-		highgui.cvQueryFrame(self.camera)
-		im = highgui.cvQueryFrame(self.camera)		
-		pilwebcam_image = opencv.adaptors.Ipl2PIL(im)
+    def __init__(self):
+        self.camera = cv.CaptureFromCAM(-1)
+        
+    
+    def apply_filter(self, image, _config):
+        im = cv.QueryFrame(self.camera)
 
-		pilwebcam_image.thumbnail(
-			(
-			int((pilwebcam_image.size[0] / 100.0) * _config['width']),
-			int((pilwebcam_image.size[1] / 100.0) * _config['height'])		
-			)
-		)
+        # HEY HERE IS SOMETHING REALLY FUNNY
+        # OPENCV USES BGR FOR IT'S INTERNAL IMAGE FORMAT
+        # WOW
+        # SO STANDARD GUYS
+        # FAN-FUCKING-TASTIC
+        # I DIDN'T TOTALLY JUST SPEND THREE HOURS WORKING THAT OUT
+        # GREAT STUFF
+        # YOUR OWN EXAMPLE CODE FOR CONVERTING TO A PIL IMAGE DOESN'T TAKE THIS INTO ACCOUNT AT ALL
+        # WTFFFFFFFFFFF
+        pilwebcam_image = Image.fromstring("RGB", cv.GetSize(im), im.tostring()[::-1]).rotate(180)
 
-		## Border
-		from ImageDraw import Draw
-		drawing = Draw(pilwebcam_image)
+        pilwebcam_image = pilwebcam_image.resize(
+            (
+            int((pilwebcam_image.size[0] / 100.0) * _config['width']),
+            int((pilwebcam_image.size[1] / 100.0) * _config['height'])      
+            ),
+            Image.ANTIALIAS
+        )
 
-		# top line
-		drawing.line(((0,0), (pilwebcam_image.size[0], 0)), fill = _config['border_colour'])
-		# left line
-		drawing.line(((0,0), (0, pilwebcam_image.size[1])), fill = _config['border_colour'])
-		# right line
-		drawing.line(((pilwebcam_image.size[0]-1,0), (pilwebcam_image.size[0]-1, pilwebcam_image.size[1])), fill = _config['border_colour'])
-		# bottow line
-		drawing.line(((0, pilwebcam_image.size[1]-1), (pilwebcam_image.size[0], pilwebcam_image.size[1]-1)), fill = _config['border_colour'])
+        ## Border
+        drawing = Draw(pilwebcam_image)
 
-		image.paste(pilwebcam_image, (10,10))
-		return image
+        # top line
+        drawing.line(((0,0), (pilwebcam_image.size[0], 0)), fill = _config['border_colour'])
+        # left line
+        drawing.line(((0,0), (0, pilwebcam_image.size[1])), fill = _config['border_colour'])
+        # right line
+        drawing.line(((pilwebcam_image.size[0]-1,0), (pilwebcam_image.size[0]-1, pilwebcam_image.size[1])), fill = _config['border_colour'])
+        # bottow line
+        drawing.line(((0, pilwebcam_image.size[1]-1), (pilwebcam_image.size[0], pilwebcam_image.size[1]-1)), fill = _config['border_colour'])
+
+        image.paste(pilwebcam_image, (10,10))
+        return image
