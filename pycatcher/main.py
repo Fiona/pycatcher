510158b4d23251554d8ad8a9e37f2b5ce8e77f29
Fiona Burrows <fiona@justfiona.com>
2011-04-28 16:00:52 +0100

    Fixed desktop capturing not working anymore

--- pycatcher/main.py
+++ pycatcher/main.py
@@ -7,176 +7,197 @@ import os, sys, time, gtk, Image
 from optparse import OptionParser
 
 config = {
-	# Path to define where to save images. Relative to where the script is run.
-	'filename_path' : 'images/',
-	# Filenames are saved as prefix%n.png where %n is the number in the sequence.
-	'filename_prefix' : 'img-',
-	# Delay in seconds between each image shot
-	'delay' : 60,
-	# List of filters to apply to the images
-	'filters' : []
+    # Path to define where to save images. Relative to where the script is run.
+    'filename_path' : 'images/',
+    # Filenames are saved as prefix%n.png where %n is the number in the sequence.
+    'filename_prefix' : 'img-',
+    # Delay in seconds between each image shot
+    'delay' : 5,
+    # List of filters to apply to the images
+    'filters' : []
 }
 
 img_counter = 1
 
 currently_paused = False
+quit = False
 
 filter_instances = {}
 
-	
+    
 def take_image():
-	""" The magic method - Captures an image and saves it out. """
+    """ The magic method - Captures an image and saves it out. """
 
-	# take screenshot
-	print "Trying to take screengrab ..."
-	
-	try:
+    # take screenshot
+    print "Trying to take screengrab ..."
 
-		img_width = gtk.gdk.screen_width()
-		img_height = gtk.gdk.screen_height()
-		
-		screengrab = gtk.gdk.Pixbuf(
-			gtk.gdk.COLORSPACE_RGB,
-			False,
-			8,
-			img_width,
-			img_height
-		)
+    try:
 
-		screengrab.get_from_drawable(
-			gtk.gdk.get_default_root_window(),
-			gtk.gdk.colormap_get_system(),
-			0, 0, 0, 0,
-			img_width,
-			img_height
-		)
+        img_width = gtk.gdk.screen_width()
+        img_height = gtk.gdk.screen_height()
+        
+        screengrab = gtk.gdk.Pixbuf(
+            gtk.gdk.COLORSPACE_RGB,
+            False,
+            8,
+            img_width,
+            img_height
+        )
+        
+        screengrab.get_from_drawable(
+            gtk.gdk.get_default_root_window(),
+            gtk.gdk.colormap_get_system(),
+            0, 0, 0, 0,
+            img_width,
+            img_height
+            )
+            
+    except:
+         die("Failed taking screenshot")
 
-	except:
-		 die("Failed taking screenshot")
+    print "Converting to PIL image ..."
 
-	print "Converting to PIL image ..."
+    final_screengrab = Image.frombuffer(
+        "RGB",
+        (img_width, img_height),
+        screengrab.get_pixels(),
+        "raw",
+        "RGB",
+        screengrab.get_rowstride(),
+        1
+    )
 
-	final_screengrab = Image.frombuffer(
-		"RGB",
-		(img_width, img_height),
-		screengrab.get_pixels(),
-		"raw",
-		"RGB",
-		screengrab.get_rowstride(),
-		1
-	)
+    for filter_name in config['filters']:
+        final_screengrab = filter_instances[filter_name].apply_filter(final_screengrab, config)
 
-	for filter_name in config['filters']:
-		final_screengrab = filter_instances[filter_name].apply_filter(final_screengrab, config)
+    print "Saving image ..."
 
-	print "Saving image ..."
+    global img_counter
+    
+    # Check what our filename should be
+    while True:
+        
+        try_file = config['filename_prefix']  + ((5 - len(str(img_counter))) * "0") + str(img_counter) + ".png"
+        
+        if os.path.exists(config['filename_path'] + try_file) == False:
+            break
 
-	global img_counter
-	
-	# Check what our filename should be
-	while True:
-		
-		try_file = config['filename_prefix']  + ((5 - len(str(img_counter))) * "0") + str(img_counter) + ".png"
-		
-		if os.path.exists(config['filename_path'] + try_file) == False:
-			break
+        img_counter += 1
 
-		img_counter += 1
+    # Save it out
+    final_screengrab.save(config['filename_path'] + try_file, "PNG")
 
-	# Save it out
-	final_screengrab.save(config['filename_path'] + try_file, "PNG")
-
-	print "Saved as %s \n" % try_file
+    print "Saved as %s \n" % try_file
 
 
-def thread_through():
-	""" Used for threading the screenshot taking mechanism so we can get input
-	And still be lazy with time.sleep() """
-	global currently_paused
+def input_thread():
+    """ Handles input. Does all the pausing. """
+    global currently_paused, quit
+    
+    while True:
+        get_input = sys.stdin.readline()
+        
+        if get_input == "\n" and not currently_paused:
+            currently_paused = True
+            print "* Paused...\n* Please enter a command. (Type exit to close PyCatcher or nothing to continue capturing.) "
 
-	#time.sleep(config['delay'])
-	
-	while True:
-		if currently_paused == False:
-			take_image()
-			time.sleep(config['delay'])
+        elif get_input in ["exit\n", "quit\n", "q\n"] and currently_paused:
+            quit = True
+            return
+
+        elif currently_paused:
+            parse_command(get_input)
+            currently_paused = False
+            print "* Unpaused...\n"
+
+
+def image_handling_thread():
+    """ Is responsible for taking images and waiting between shots. """
+    global currently_paused, quit
+    
+    while True:
+        if currently_paused == False:
+            take_image()
+            for x in range(config['delay']):
+                if quit:
+                    return
+                time.sleep(1)
 
 
 def load_configuration(filename):
-	""" Overrides the configuration values within the defined file """
-	config_mod = __import__(filename)
-	config.update(config_mod.config)
+    """ Overrides the configuration values within the defined file """
+    config_mod = __import__(filename)
+    config.update(config_mod.config)
 
 
 def parse_command(command):
-	pass
+    pass
 
 
 def main():
-	""" Main entry point """
-	parser = OptionParser()
+    """ Main entry point """
+    parser = OptionParser()
 
-	parser.add_option("-c", "--config", dest="config_filename",
-					  default="defaults",
-					  help="Specify name of configuration file. [Default: %default]")
+    parser.add_option("-c", "--config", dest="config_filename",
+                      default="defaults",
+                      help="Specify name of configuration file. [Default: %default]")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	if os.path.exists(options.config_filename + ".py") == False:
-		parser.error("Configuration file not found.")
+    if os.path.exists(options.config_filename + ".py") == False:
+        parser.error("Configuration file not found.")
 
-	load_configuration(options.config_filename)
+    load_configuration(options.config_filename)
 
-	if os.path.exists(config['filename_path']) == False:
-		sys.exit("Path defined in configuration does not exist.")
+    if os.path.exists(config['filename_path']) == False:
+        sys.exit("Path defined in configuration does not exist.")
 
-	# load any overriding filter configs
-	for filter_name in config['filters']:
-		fil = __import__("filters." + filter_name)
-		filter_instances[filter_name] = fil.__dict__[filter_name].image_filter()
-		
-		filter_config = filter_instances[filter_name].config
+    # load any overriding filter configs
+    for filter_name in config['filters']:
+        fil = __import__("filters." + filter_name)
+        filter_instances[filter_name] = fil.__dict__[filter_name].image_filter()
+        
+        filter_config = filter_instances[filter_name].config
 
-		for k in filter_config:
-			if k not in config:
-				config[k] = filter_config[k]
-			
+        for k in filter_config:
+            if k not in config:
+                config[k] = filter_config[k]
+            
 
-	print "---------------------------"
-	print "PyCatcher -----------------"
-	print "---------------------------"
-	print "By Fiona Burrows ----------"
-	print "---------------------------"
-	print "<fiona@myrmidonprocess.com-\n"
+    print "---------------------------"
+    print "PyCatcher -----------------"
+    print "---------------------------"
+    print "By Fiona Burrows ----------"
+    print "---------------------------"
+    print "<fiona@myrmidonprocess.com-\n"
 
-	print "* Hit enter to pause capturing and/or enter a command.\n"
-	print "* Using configuration file at %s.py" % options.config_filename
-	print "* Starting capture thread... \n"
-	
-	import thread
-	thread.start_new_thread(thread_through, ())
+    print "* Hit enter to pause capturing and/or enter a command.\n"
+    print "* Using configuration file at %s.py" % options.config_filename
+    print "* Starting capture thread... \n"
 
-	global currently_paused
-	
-	while True:
-		get_input = raw_input("")
-		currently_paused = True
-		
-		get_command = raw_input("* Paused...\n* Please enter a command. (exit to close PyCatcher or nothing to continue.)\n: ")
+    from threading import Thread
 
-		if get_command in ["exit", "quit", "q"]:
-			break
+    #import thread
+    #thread.start_new_thread(thread_through, ())
+    input_thread_handle = Thread(target=input_thread, args=())
+    image_handling_thread_handle = Thread(target=image_handling_thread, args=())
 
-		if get_command != "":
-			parse_command(get_command)
+    input_thread_handle.start()
+    image_handling_thread_handle.start()
+    
+    global currently_paused, quit
+    
+    while True:
+        if quit:
+            break
 
-		currently_paused = False
-		
-		print "* Unpaused...\n"
-		
-	sys.exit("Quit!")
-	
+    # Wait for threads to die
+    input_thread_handle.join()
+    image_handling_thread_handle.join()
+    
+    sys.exit("Quit!")
+    
 
 if __name__ == "__main__":
-	main()
+    main()
 
